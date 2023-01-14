2fb6b3955560d6c57d186e57c7c8c97ee184b2b4
Debarghya-Basak <106986982+Debarghya-Basak@users.noreply.github.com>
2023-01-14 23:33:49 +0530

    Added basic movements and burn effect

---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RocketController : MonoBehaviour
+{
+    Vector2 moveAxis;
+    float moveSpeed;
+    Rigidbody2D rocketRigidBody;
+    SpriteRenderer burnEffectRenderer;
+
+    [SerializeField] GameObject burnEffect;
+    [SerializeField] Sprite rocketBurnEffectLowPower;
+    [SerializeField] Sprite rocketBurnEffectHighPower;
+
+    void Start(){
+        moveAxis = new Vector2(0,0);
+        rocketRigidBody = GetComponent<Rigidbody2D>();
+        moveSpeed = 2f;
+        
+        burnEffectRenderer = burnEffect.GetComponent<SpriteRenderer>();
+        burnEffectRenderer.sprite = rocketBurnEffectLowPower;
+    }
+
+    void Update(){
+        move();
+    }
+
+    void OnMove(InputValue move){
+
+        moveAxis = move.Get<Vector2>();
+
+        Debug.Log(moveAxis);
+
+    }
+
+    void move(){
+
+        if(moveAxis.x != 0f || moveAxis.y != 0f){
+            rocketRigidBody.drag = 0f;
+            rocketRigidBody.velocity = new Vector2((moveAxis.x * moveSpeed), (moveAxis.y * moveSpeed));
+            burnEffectRenderer.sprite = rocketBurnEffectHighPower;
+        }
+        else{
+            rocketRigidBody.drag = 5f;  
+            burnEffectRenderer.sprite = rocketBurnEffectLowPower;
+        }
+            
+            
+
+    }
+}
